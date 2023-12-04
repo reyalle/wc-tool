4a1b279e765a476771f356de4b942a42261285a0
reyalle <reyxeka@gmail.com>
2023-12-04 13:30:40 -0500

    added functionality for word count

--- ccwc.py
+++ ccwc.py
@@ -24,12 +24,28 @@ def count_lines(file_path):
         print(f"Error: {e}")
 
 
+def count_words(file_path):
+    try:
+        word_count = 0
+        with open(file_path, 'r') as file:
+            for line in file:
+                words = line.split()
+                word_count += len(words)
+            print(f"{word_count} {file_path}")
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
 def main():
     parser = argparse.ArgumentParser(description='wc tool for word line character and byte count.')
     parser.add_argument('-c', '--count', metavar='FILE', type=str,
                         help='The file path requested to run byte count command')
     parser.add_argument('-l', '--lines', metavar='FILE', type=str,
                         help="File for which to count lines")
+    parser.add_argument('-w', '--words', metavar='FILE', type=str,
+                        help='File for which to count words')
 
     args = parser.parse_args()
 
@@ -37,6 +53,8 @@ def main():
         count_bytes(args.count)
     elif args.lines:
         count_lines(args.lines)
+    elif args.words:
+        count_words(args.words)
     else:
         print("Error: Please provide a file path with either '-c' or '-l' option.")
 
